30d0ffa6979d2156c059ed0c37dfe3391fa11268
Shahul Alam <alam.shahul@gmail.com>
2022-04-09 08:48:41 -0400

    Changes to server to get image generation working.

--- server/api/HelloApiHandler.py
+++ server/api/HelloApiHandler.py
@@ -1,6 +1,7 @@
 from flask import request
 from flask_restful import Api, Resource, reqparse
-from api.process_image import write_base64_as_png
+from api.process_image import write_base64_as_png, crop, reimagine
+from pathlib import Path
 
 class HelloApiHandler(Resource):
   def get(self):
@@ -13,9 +14,16 @@ class HelloApiHandler(Resource):
     print(dir(self))
     json_data = request.get_json(force=True)
     
+    text_prompt = 'the letters C, P, C and B with DNA wrapped around them'
+    text_prompt = json_data["prompt"]
     image_data = json_data["image"]
     image_format, base64_image = image_data.split(",")
-    write_base64_as_png(base64_image)
+    filepath = Path("temp.png")
+    cropped_filepath = Path("cropped_temp.png")
+    write_base64_as_png(base64_image, filepath)
+    crop(filepath, cropped_filepath)
+    reimagine(text_prompt, cropped_filepath, mode="steer")
+
     ret_msg = json_data
 
     if ret_msg:
--- server/api/process_image.py
+++ server/api/process_image.py
@@ -1,7 +1,63 @@
 import base64
+from deep_daze import Imagine
+from PIL import Image
 
 # https://stackoverflow.com/questions/5368669/convert-base64-to-image-in-python
-def write_base64_as_png(png_b64text):
+def write_base64_as_png(png_b64text, filepath):
     png_recovered = base64.b64decode(png_b64text)
-    with open("temp.png", "wb") as f:
+    with open(filepath, "wb") as f:
         f.write(png_recovered)
+
+def crop(filepath, new_filepath):                                                                                                                                                                                                                             
+    im = Image.open(filepath)
+    width, height = im.size   # Get dimensions
+        
+    new_width = 256 
+    new_height = 256 
+        
+    left = (width - new_width)/2
+    top = (height - new_height)/2
+    right = (width + new_width)/2
+    bottom = (height + new_height)/2
+        
+    # Crop the center of the image
+    im = im.crop((left, top, right, bottom))
+        
+    im.save(new_filepath)
+
+def reimagine(text, filepath, mode="refine"):
+	"""Use deep-daze to reimagine a logo
+
+	"""
+	if mode == "refine":
+		im = Image.open(filepath)
+		imagine = Imagine(
+		    # text = text,
+		        num_layers = 24,
+		        image_width = 256,
+		        epochs = 8,
+				img = im,
+		        # start_image_path = filepath,
+				# start_image_train_iters=10,
+				save_progress=True,
+				save_gif=True,
+				iterations=200,
+		)
+	
+	elif mode == "steer":
+		imagine = Imagine(
+		    	text = text,
+		        num_layers = 48,
+		        image_width = 256,
+		        epochs = 10,
+				batch_size=64,
+                gradient_accumulate_every=1,
+		        start_image_path = filepath,
+				start_image_train_iters=20,
+				save_progress=True,
+				save_gif=True,
+				iterations=500,
+		)
+
+
+	imagine()
